c4dee4d9da1d419dbd562cf8f4c775a5b0e68fd4
Juan M Barrera <ju.ma.barrera.rivera@unillanos.edu.co>
2024-11-01 02:32:46 -0600

    DE nuevo todo en un solo archivo main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,315 @@
 #include <iostream>
 #include <vector>
 #include <exception>
-#include "epersonalizado.h"
-#include "pokemon.h"
-#include "entrenador.h"
-#include "batalla.h"
-
 using namespace std;
+
+class Epersonalizado : public exception {
+private:
+    const char *msg;
+    const char *causa;
+public:
+    Epersonalizado(const char *mensaje, const char *cau) : msg(mensaje), causa(cau) {}
+    const char *what() const noexcept override{
+        cerr << "causa : " << causa << endl;
+        return msg;
+    }
+};
+class Pokemon
+{
+private:
+    string nombre;
+    string color;
+    int ataque;
+    int vida;
+    int xp;
+    int nivel;
+    string ataqueEspecial;
+
+public:
+    Pokemon(): nombre("Pokemon"), color("amarillo"), ataque(20), vida(100), xp(0), nivel(1), ataqueEspecial("") {}
+
+    Pokemon(string nombre, string color, int ataque, int vida, int xp, int nivel, string ataqueEspecial)
+    {
+        this->nombre = nombre;
+        this->color = color;
+        this->ataque = ataque;
+        this->vida = vida;
+        this->xp = xp;
+        this->nivel = nivel;
+        this->ataqueEspecial = ataqueEspecial;
+    }
+
+    string getNombre() const { return nombre; }
+    string getColor() const { return color; }
+    string getAtaqueEspecial() const { return ataqueEspecial; }
+    int getAtaque() const { return ataque; }
+    int getVida() const { return vida; }
+    int getXp() const { return xp; }
+    int getNivel() const { return nivel; }
+
+    void setNombre(string nombre) { this->nombre = nombre; }
+    void setColor(string color) { this->color = color; }
+    void setAtaque(int ataque) { this->ataque = ataque; }
+    void setVida(int vida) { this->vida = vida; }
+    void setXp(int xp) { this->xp = xp; }
+    void setNivel(int nivel) { this->nivel = nivel; }
+    void setAtaqueEspecial(string ataqueEspecial) { this->ataqueEspecial = ataqueEspecial; }
+
+    void saludar()
+    {
+        cout << "¡Hola!, soy " << nombre << ", estoy a tu disposición." << endl;
+        mostrarDatos();
+    }
+
+    void sanar()
+    {
+        if (vida < 100)
+        {
+            vida = 100;
+        }
+    }
+
+    void evolucionar(string nombre, string color)
+    {
+        if (nivel == 2)
+        {
+            this->nombre = nombre;
+            this->color = color;
+            ataque += ataque * 0.2;
+        }
+        else if (nivel == 3)
+        {
+            this->nombre = nombre;
+            this->color = color;
+            ataque += ataque * 0.2;
+        }
+        mostrarDatos();
+    }
+    bool verificarAumentoNivel()
+    {
+        return xp > nivel * 100;
+    }
+
+    void aumentarNivel()
+    {
+        nivel++;
+    }
+
+    void mostrarDatos()
+    {
+        cout << nombre << " Color: " << color << " || Nivel de Vida: " << vida << " || Potencia de Ataque: " << ataque << " || Xp: " << xp << " || Nivel: " << nivel << endl;
+    }
+
+    // sobrecarga del + para crear pokemon recargado
+    Pokemon operator+(const Pokemon &c) const
+    {
+        // creamos un objeto nuevo de la clase cargo llamado "pokemonrecargado" e instantaneamente en sus parametros
+        // sumamos los atributos de otros objetos para agregarlos en los atributos de "pokemonrecargado"
+        // y luego se retorna el objeto
+        Pokemon pokemonrecargado(" SuperPokemon! ", " multicolor! ", ataque + c.ataque, vida + c.vida, xp + c.xp, nivel + c.nivel, "Vacio infinito!");
+
+        return pokemonrecargado;
+    }
+    bool operator==(const Pokemon &p) const {
+        return (nombre == p.nombre &&
+                color == p.color &&
+                ataque == p.ataque &&
+                vida == p.vida &&
+                xp == p.xp &&
+                nivel == p.nivel &&
+                ataqueEspecial == p.ataqueEspecial);
+    }
+    friend ostream &operator<<(ostream &o, Pokemon p);
+};
+ostream &operator<<(ostream &o, Pokemon p)
+{
+    return o << "\nNombre: " << p.getNombre() << " \n color: " << p.getColor() << "\n ataque: " << p.getAtaque() << "\n vida " << p.getVida() << " \n experiencia: " << p.getXp() << "\n nivel: " << p.getNivel() << "\n ataque especial: " << p.getAtaqueEspecial() << endl;
+}
+
+class Entrenador {
+private:
+    vector<Pokemon> misPokemones; // aunque debe ser 3, eso se maneja en la excepción
+    string nombre;
+    int nivel;
+
+public:
+    Entrenador() : nombre(""), nivel(1) {}
+
+    string getNombre() const { return nombre; }
+    int getNivel() const { return nivel; }
+    void setNombre(string nombre) { this->nombre = nombre; }
+    void setNivel(int nivel) { this->nivel = nivel; }
+
+    Pokemon &getPokemon(int i) {
+        return misPokemones[i];
+    }
+
+    void agregarpokemon(Pokemon &poke) {
+        if (misPokemones.size() < 3) {
+            misPokemones.push_back(poke);
+        } else {
+            throw Epersonalizado("Error al agregar el pokemon", "El limite son 3 pokemones");
+        }
+    }
+
+    void entrenar(Pokemon &miPok, const Pokemon &oponente) {
+        miPok.setXp(miPok.getXp() + oponente.getNivel() * nivel);
+        cout << "El entrenador " << nombre << " ha entrenado a " << miPok.getNombre() << endl;
+        if (miPok.verificarAumentoNivel()) {
+            string nombre, color;
+            // Cambiar mensajes
+            cout << "¡Enhorabuena, has subido de nivel!!\nPuedes cambiar el nombre y color de tu Pokémon. Además, la potencia de ataque se aumentó un 20%\n";
+            cout << "Nuevo nombre:\n";
+            cin >> nombre;
+            cout << "Nuevo color:\n";
+            cin >> color;
+            miPok.aumentarNivel();
+            miPok.evolucionar(nombre, color);
+            aumentarNivel();
+        }
+        cout << miPok;
+    }
+
+    void aumentarNivel() {
+        setNivel(getNivel() + 1);
+    }
+    //const despues de parentesis
+    bool operator==(const Entrenador &ec) const{
+        if (nombre != ec.nombre || nivel != ec.nivel)
+            return false;
+        if (misPokemones.size() != ec.misPokemones.size())
+            return false;
+        for (int i = 0; i < misPokemones.size(); ++i) {
+            if (!(misPokemones[i] == ec.misPokemones[i]))
+                return false;
+        }
+        return true;
+    }
+    friend ostream &operator<<(ostream &o, Entrenador c);
+};
+ostream &operator<<(ostream &o, Entrenador c) {
+    for (const auto &poke : c.misPokemones) {
+        o << poke << " ";
+    }
+    return o;
+}
+
+class Batalla {
+private:
+    int registroDaño;
+    vector<string> ganadoresCombatesPok;
+    string contrincantes[2];
+    string ganador;
+    vector<int> registroRondas;
+
+public:
+    Batalla(): registroDaño(0) {}
+    Batalla(string ent1, string ent2) {
+        contrincantes[0] = ent1;
+        contrincantes[1] = ent2;
+        registroDaño = 0;
+    }   
+    int getRegistroDaño() const { return registroDaño; }
+    string getGanador() const { return ganador; }
+    string getContrincante(int i) const { return contrincantes[i]; }
+    string getGanadoresCombatesPok(int i) { return ganadoresCombatesPok[i]; }
+
+    void setGanador(Entrenador &en) { ganador = en.getNombre(); }
+    void setRegistroDaño(int registroDaño) { this->registroDaño = registroDaño; }
+
+    void agregarPokGanador(Pokemon &pok) {
+        ganadoresCombatesPok.push_back(pok.getNombre());
+    }
+
+    void agregarRondas(int rond) {
+        registroRondas.push_back(rond);
+    }
+
+    void luchar(Pokemon &miPok, Pokemon &oponente, bool &ganador) {
+        int vidaOponente, vidaMiPok, ataqueMiPok, ataqueOponente, nivelMipok, nivelOponente, xpMiPok, xpOponente, ronda = 1, atq = 0;
+        char sig;
+        vidaOponente = oponente.getVida();
+        vidaMiPok = miPok.getVida();
+        ataqueMiPok = miPok.getAtaque();
+        ataqueOponente = oponente.getAtaque();
+        nivelMipok = miPok.getNivel();
+        nivelOponente = oponente.getNivel();
+        cout << "\n\n\n\n QUE COMIENCE LA PELEA!\n ";
+        miPok.mostrarDatos();
+        cout << "\n          vs          \n";
+        oponente.mostrarDatos();
+        cout << "\n\n***********\n\n";
+        cout << "Presiona (x) para lanzar un ataque normal o (p) para un ataque especial." << endl;
+        cout << "Solo tienes una oportunidad para lanzar el ataque especial" << endl;
+        while (vidaOponente > 0 && vidaMiPok > 0) {
+            int restOp, restMi;
+            char sig, tipoAtaque;
+            cout << "Ronda " << ronda << endl; // Inicia ronda
+            if (ronda % 3 == 0 || vidaOponente < 40) {
+                vidaMiPok -= (ataqueOponente + ataqueOponente * .3);
+                setRegistroDaño(getRegistroDaño() + (ataqueOponente + ataqueOponente * .4));
+            } else {
+                vidaMiPok -= ataqueOponente;
+                setRegistroDaño(getRegistroDaño() + ataqueOponente);
+            }
+            miPok.setVida(vidaMiPok); 
+            // Verifica si sigue vivo
+            if (vidaMiPok <= 0) {
+                vidaMiPok = 0;
+                ganador = false; // Establece ganador
+                break;
+            }
+            bool cond;
+            do {
+                try {
+                    cout << "Ataca!!" << endl;
+                    cin >> tipoAtaque;
+                    if (tipoAtaque == 'x') {
+                        vidaOponente -= ataqueMiPok;
+                        setRegistroDaño(getRegistroDaño() + ataqueMiPok);
+                        cond = false;
+                    } else if (atq == 0 && tipoAtaque == 'p') {
+                        vidaOponente -= (ataqueMiPok + ataqueMiPok * .4);
+                        setRegistroDaño(getRegistroDaño() + (ataqueMiPok + ataqueMiPok * .4));
+                        atq++;
+                        cout << miPok.getNombre() << " lanzó su ataque especial de " << miPok.getAtaqueEspecial() << endl;
+                        cond = false;
+                    } else if(atq != 0 && tipoAtaque == 'p') {
+                        throw Epersonalizado("Error al lanzar el ataque", "Ataque especial ya usado");
+                    } else {
+                        throw Epersonalizado("Opcion no valida", "Escoger otra opcion");
+                    }
+                } catch (Epersonalizado &e) {
+                    cerr << e.what() << endl;
+                    cout << "Intenta otro ataque" << endl;
+                    cond = true;
+                    //continue;
+                }
+            } while (cond);
+            cout << miPok.getNombre() << ": Vida: " << vidaMiPok << endl;
+            cout << oponente.getNombre() << ": Vida: " << vidaOponente << endl;
+            cout << "\n\n*********\n\n";
+            do {
+                cout << "Presiona (n) para continuar" << endl;
+                cin >> sig;
+            } while (sig != 'n' && sig != 'N');
+            ronda++;
+        }
+        miPok.sanar();
+        oponente.sanar();
+        agregarRondas(ronda);
+    }
+};
+
+ostream &operator<<(ostream &o, Batalla c) {
+    return o << c.getContrincante(0) << " vs " << c.getContrincante(1) << "\n"
+             << "Daño en total que se hicieron los Pokemones: " << c.getRegistroDaño() << "\n"
+             << "Ganador Combate 1:" << c.getGanadoresCombatesPok(0) << "\n"
+             << "Ganador Combate 2:" << c.getGanadoresCombatesPok(1) << "\n"
+             << "Ganador Combate 3:" << c.getGanadoresCombatesPok(3) << "\n"
+             << "\nEntrenador Ganador: " << c.getGanador() << endl;
+}
+
 
 Batalla lucharEntrenadores(Entrenador &rude1, Entrenador &rude2);
 
